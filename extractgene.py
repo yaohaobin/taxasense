95dfb65a77f3684a7893fe1178a149bb187abb87
hbyao <hbyao@bal6.cs.hku.hk>
2018-08-09 18:55:19 +0800

    commit

--- extractgene.py
+++ extractgene.py
@@ -5,22 +5,24 @@ from Bio.SeqFeature import SeqFeature
 from Bio.SeqRecord import SeqRecord
 import os.path
 def parsegbk(filename):
-    gbfile = open(sys.argv[1],'r')
+    gbfile = open(filename,'r')
   
     for record in SeqIO.parse(gbfile,'genbank'):
 	    for f in record.features:
 		    if f.type == 'CDS':
 			    if 'gene' in f.qualifiers:
 				    protid = f.qualifiers['protein_id'][0]
-			        geneid = f.qualifiers['gene'][0]
+			            geneid = f.qualifiers['gene'][0]
 				    print protid+'\t'+geneid+'\t'+str(len(f.location.extract(record)))
 
     gbfile.close()
 
 def processdir(args,dirname,filenames):
 	for filename in filenames:
-		print dirname+'/'+filename
-		parsegbk(dirname+'/'+filename)
+                info = filename.rstrip().split('.')
+                if info[len(info)-1] == 'gbk':
+		    print dirname+'/'+filename
+		    parsegbk(dirname+'/'+filename)
 
 os.path.walk(sys.argv[1],processdir,None)
 
