9c953580bf465c7437fd8db9e0618c7196c99439
hbyao <hbyao@bal6.cs.hku.hk>
2018-08-11 23:55:47 +0800

    commit

--- extractgene.py
+++ extractgene.py
@@ -3,21 +3,33 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqFeature import SeqFeature
 from Bio.SeqRecord import SeqRecord
-import os.path
+import os
 from multiprocessing import Pool
 
 def parsegbk(filename):
+    print filename
+    info = filename.split('/')
+    lastname = info[len(info)-1]
+    #outfile = open(sys.argv[2]+'/'+lastname,'w')
+
     gbfile = open(filename,'r')
-  
+    outstr = filename+'\n'  
     for record in SeqIO.parse(gbfile,'genbank'):
 	    for f in record.features:
 		    if f.type == 'CDS':
 			    if 'gene' in f.qualifiers:
 				    protid = f.qualifiers['protein_id'][0]
 			            geneid = f.qualifiers['gene'][0]
-				    print protid+'\t'+geneid+'\t'+str(len(f.location.extract(record)))
+				    outstr = outstr + protid+'\t'+geneid+'\t'
+                                    outstr = outstr + f.location.extract(record).seq+'\n'
 
     gbfile.close()
+    print lastname,len(outstr)
+    outfile = open(sys.argv[2]+'/'+lastname,'w')
+    outfile.write(str(outstr))
+    outfile.flush()
+    outfile.close()
+    return lastname
 
 def processdir(args,dirname,filenames):
 	for filename in filenames:
@@ -28,19 +40,35 @@ def processdir(args,dirname,filenames):
 
 
 def job(filename):
-	return filename
-
+           
+        return parsegbk(filename)
+	
+def outputresult(result):
+    print result[0]
+    outfile = open(sys.argv[2]+'/'+result[0],'w')
+    outfile.write(str(result[1]))
+    outfile.close()
 
 gbklist = []
-for fpath,dirname,filename in os.path.walk(sys.argv[1]):
-	info = filename.rstrip().split('.')
-	if info[len(info)-1] == 'gbk':
-		gbklist.append(dirname+'/'+filename)
+for fpath,dirname,filenames in os.walk(sys.argv[1]):
+        for filename in filenames:
+	    info = filename.rstrip().split('.')
+	    if info[len(info)-1] == 'gbk':
+		    gbklist.append(fpath+'/'+filename)
 print len(gbklist)
 
-p = Pool(6)
+p = Pool(12)
 
-results = p.map(job,gbklist)
+idx = 0
+p.map_async(job,gbklist)
+p.close()
+p.join()
+#print len(results)
 
-print len(results)
-print results[0]
+
+    
+    #outfile = open(sys.argv[2]+'/'+lastname,'w')
+    #outfile.write(results[idx].get())
+    #outfile.close()
+   
+
